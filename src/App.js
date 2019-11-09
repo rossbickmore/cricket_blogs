23d5d60a98fa19a7ae9c317495dcbfbe77032af4
ROSS BICKMORE <rossbickmore@gmail.com>
2019-11-09 16:49:10 +0000

    added server, get and post requests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+
 
 function App() {
+  const [blogs, setBlogs] = useState([])
+  const [author, setAuthor] = useState("")
+  const [content, setContent] = useState("")
+  const [title, setTitle] = useState("")
+
+  const url = "http://localhost:3000/blogs"
+  
+  useEffect(() => {
+    fetch(url)
+      .then( response => response.json())
+      .then( data => setBlogs(data))
+  })
+
+  const updateAuthor = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+    setAuthor(e.target.value)
+  }
+
+  const updateContent = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+    setContent(e.target.value)
+  }
+
+  const updateTitle = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+    setTitle(e.target.value)
+  }
+
+  const addBlog = e => {
+    e.preventDefault()
+    const newBlog = { 
+      author: author,
+      content: content,
+      title: title
+    }
+    const options = {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" }, 
+      body: JSON.stringify(newBlog),
+    }
+    fetch(url, options)
+    .then((res) => res.json())
+    .then(data =>console.log(data))
+    setAuthor("")
+    setContent("")
+  }
+
+
   return (
     <div className="App">
-      Hello world!
+      <h1>
+        Cricket Blogs
+      </h1>
+      <ul>
+        {blogs.map((blog) => 
+        <li key={blog.id}>
+          {blog.title} - a blog created by {blog.author}
+          <p>{blog.content}</p>
+        </li>
+        )}
+      </ul>
+      <h2>Add blogs</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          <input type="text" placeholder="author's name" onChange={updateAuthor} value={author}/>
+        </div>
+        <div>
+          <input type="text" placeholder="title" onChange={updateTitle} value={title}/>
+        </div>
+        <div>
+        <textarea placeholder="content" onChange={updateContent} value={content}/>
+        </div>
+        <div>
+        <input type="submit"/>
+        </div>
+      </form>
     </div>
   );
 }
