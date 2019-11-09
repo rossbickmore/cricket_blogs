dd3854ab5ad0ab6ba2ba8b8cb570cdb715b05c27
ROSS BICKMORE <rossbickmore@gmail.com>
2019-11-09 18:03:33 +0000

    added delete and update crud operation

--- src/App.js
+++ src/App.js
@@ -38,7 +38,8 @@ function App() {
     const newBlog = { 
       author: author,
       content: content,
-      title: title
+      title: title,
+      important: true
     }
     const options = {
       method: 'POST',
@@ -50,7 +51,39 @@ function App() {
     .then(data =>console.log(data))
     setAuthor("")
     setContent("")
+    setTitle("")
   }
+
+  const deleteBlog = (id) => {
+    fetch(url + "/" + id, {
+      method: 'DElETE'
+    })
+      .then( response => response.json())
+      .then( data => console.log(data))
+    blogs.filter(blog => blog.id !== id)
+    console.log(id)
+  }
+
+  const toggleImportance = (id) => {
+    const blogToUpdate = blogs.find(blog => blog.id === id)
+    const updatedBlog = {
+      ...blogToUpdate,
+      important: !blogToUpdate.important
+    }
+    console.log("old blog", blogToUpdate)
+    console.log("new blog", updatedBlog)
+    fetch(url + "/" + id, {
+      method: 'PUT',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedBlog)
+    })
+      .then( response => response.json())
+      .then( data => console.log(data))
+    blogs.map(blog => blog.id === id ? updatedBlog : blog)
+    console.log(id)
+  }
+  
+  
 
 
   return (
@@ -61,8 +94,10 @@ function App() {
       <ul>
         {blogs.map((blog) => 
         <li key={blog.id}>
-          {blog.title} - a blog created by {blog.author}
+          {blog.title} - a blog created by {blog.author} {blog.importance}
           <p>{blog.content}</p>
+          <button onClick={() => deleteBlog(blog.id)}>Delete blog</button>
+          <button onClick={() => toggleImportance(blog.id)}>Toggle importance</button>
         </li>
         )}
       </ul>
@@ -75,10 +110,10 @@ function App() {
           <input type="text" placeholder="title" onChange={updateTitle} value={title}/>
         </div>
         <div>
-        <textarea placeholder="content" onChange={updateContent} value={content}/>
+          <textarea placeholder="content" onChange={updateContent} value={content}/>
         </div>
         <div>
-        <input type="submit"/>
+          <input type="submit"/>
         </div>
       </form>
     </div>
